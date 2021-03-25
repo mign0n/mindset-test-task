8336a7a4a3444696aefbc50af60c6078866dce3e
mign0n <56875363+mign0n@users.noreply.github.com>
2021-03-25 09:55:43 +0500

    Added the ability to calculate the probability of contract renewal. A small refactoring was carried out.

--- models.py
+++ models.py
@@ -12,20 +12,37 @@ class Predictor:
         self.classifier.load_model(model_path)
         self.data_path = data_path
         self.data = None
+        self.data_pool = None
 
-    def _predict(self):
-        cat_features = self.data.select_dtypes(exclude=[np.number]).columns
-        data_pool = Pool(data=self.data.astype('str'), cat_features=cat_features)
-        return self.classifier.predict(data_pool)
-
-    def _read_data(self, sep):
+    def _read_data_to_pool(self, sep):
+        if isinstance(self.data, pd.DataFrame):
+            return self.data
         with open(self.data_path, 'r') as file:
             self.data = pd.read_csv(file, sep=sep)
-        return self.data
+            cat_features = self.data.select_dtypes(exclude=[np.number]).columns
+            self.data_pool = Pool(
+                data=self.data.astype('str'), cat_features=cat_features
+            )
+        return self.data_pool
+
+    def _predict(self):
+        return self.classifier.predict(self.data_pool)
+
+    def _predict_proba(self):
+        probabilities = self.classifier.predict_proba(self.data_pool)
+        return probabilities[:, 1]
 
     def get_forecast(self):
-        self._read_data(sep=',')
+        if isinstance(self.data_pool, Pool):
+            return self._predict()
+        self._read_data_to_pool(sep=',')
         return self._predict()
+
+    def get_probability(self):
+        if isinstance(self.data_pool, Pool):
+            return self._predict_proba()
+        self._read_data_to_pool(sep=',')
+        return self._predict_proba()
 
 
 predictor = Predictor(MODEL_PATH, DATA_PATH)
